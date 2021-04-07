e24ef0138bd46d476013cc2547e91756fd80ecf3
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-04-07 15:49:59 +0530

    Sets added

---
+++ src/hashtable/FirstUniqueChar.java
@@ -0,0 +1,36 @@
+package hashtable;
+
+import java.util.*;
+
+public class FirstUniqueChar {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // passing string
+        String str = scanner.next();
+        scanner.close();
+
+        char[] strArray = str.toCharArray();
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i=0; i<strArray.length; i++) {
+            char key = strArray[i];
+            if (map.containsKey(key)) {
+                map.put(key,map.get(key)+1);
+            } else {
+                map.put(key,1);
+            }
+        }
+
+        for (int i=0; i<strArray.length; i++){
+            if (map.get(strArray[i]) == 1) {
+                System.out.println(strArray[i]);
+                break;
+            }
+        }
+
+    }
+    
+}
---
+++ src/hashtable/TreeSetEx.java
@@ -0,0 +1,28 @@
+package hashtable;
+
+import java.util.TreeSet;
+
+/**
+ *  
+ * A TreeSet stores elements in an ascending order and does not store any duplicate elements.
+ * 
+ * */
+
+public class TreeSetEx {
+
+    public static void main(String args[]) {
+        // create a TreeSet named "tSet"
+        TreeSet<String> tSet = new TreeSet<>();
+
+        // adding elements to it
+        tSet.add("4");
+        tSet.add("2");
+        tSet.add("7");
+        tSet.add("5");
+        tSet.add("9");
+
+        // printing the TreeSet
+        System.out.println(tSet);
+    }
+    
+}
