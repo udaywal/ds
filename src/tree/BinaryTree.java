c3aeb307a16af54094db0fffb0937c94c382bc28
Vaibhav Udaywal <vaibhav.udaywal@otssolutions.com>
2021-04-10 04:24:01 +0530

    binary tree dfs and bfs added

--- src/tree/BinaryTree.java
+++ src/tree/BinaryTree.java
@@ -77,12 +77,26 @@ class Tree {
         }
 	}
 
+    // Method to convert a given binary tree into its mirror image
+	public void mirror(Node node) {
+		if (node == null) {
+            return;
+        } else {
+            mirror(node.left);
+            mirror(node.right);
+            Node temp = node.right;
+            node.right = node.left;
+            node.left = temp;
+        }
+	}
+
     /* Through BFS (level traversal) */
     void traverseBFS(Node node) {
         Queue<Node> queue = new LinkedList<Node>();
         queue.add(node);
         while (!queue.isEmpty()) {
             Node temp = queue.peek();
+            queue.remove();
             System.out.print(temp.data + " ");
             // enqueue left child into queue
             if (temp.left != null)
@@ -93,9 +107,38 @@ class Tree {
         }
     }
 
+    /** 
+     * You are given a binary tree and are expected to print the spiral order traversal of the tree, 
+     * which is equivalent to the level order traversal of the tree in a zig-zag order. 
+     * For the tree given below, the spiral order traversal is 1, 2, 3, 7, 6, 5, 4, 8, 9. 
+     * */
+    void spiralLevelOrderTraversal(Node node) {
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.add(node);
+        boolean flag = false;
+        while (!queue.isEmpty()) {
+            Node temp = queue.peek();
+            queue.remove();
+            System.out.print(temp.data + " ");
+            flag = !flag;
+            if (temp.left != null)
+                if (flag) {
+                    queue.add(temp.left);
+                } else {
+                    queue.add(temp.right);
+                }
+            if (temp.right != null)
+                if (flag) {
+                    queue.add(temp.right);
+                } else {
+                    queue.add(temp.left);
+                }
+        }
+    }
+
 }
 
-public class BinaryTreeDFSBFS {
+public class BinaryTree {
 
     public static void main(String[] args) {
 
@@ -112,17 +155,23 @@ public class BinaryTreeDFSBFS {
         tree.root.right.left = new Node(6);
         tree.root.right.right = new Node(7);
 
-        System.out.println("\nPre-order traversal : ");
-        tree.preOrderDFS(tree.root);
+        // System.out.println("\nPre-order traversal : ");
+        // tree.preOrderDFS(tree.root);
 
-        System.out.println("\nIn-order traversal : ");
-        tree.inOrderDFS(tree.root);
+        // System.out.println("\nIn-order traversal : ");
+        // tree.inOrderDFS(tree.root);
 
-        System.out.println("\nPost-order traversal : ");
-        tree.postOrderDFS(tree.root);
+        // System.out.println("\nPost-order traversal : ");
+        // tree.postOrderDFS(tree.root);
         
-        System.out.println("\nHeight of binary tree : " + tree.findHeight(tree.root));;
+        // System.out.println("\nHeight of binary tree : " + tree.findHeight(tree.root));
         
+        // System.out.println("\nMirror tree : ");
+        // tree.mirror(tree.root);
+        // tree.preOrderDFS(tree.root);
+        
+        System.out.println("\nSpiral order traversal : ");
+        tree.spiralLevelOrderTraversal(tree.root);
     }
     
 }
